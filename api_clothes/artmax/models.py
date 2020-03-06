850cc4f8ee843bdf168dfcac98823b9ed007f3a5
Rustam Kayumov <rustrnb@yandex.ru>
2020-03-06 09:24:44 +0500

    'fixed_unique'

--- api_clothes/artmax/models.py
+++ api_clothes/artmax/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Products(models.Model):
-   name = models.CharField(max_length=150)
-   
+   name = models.CharField(max_length=150, verbose_name='Наименование', unique=True)
+
 
    def __str__(self):
    	return self.name
